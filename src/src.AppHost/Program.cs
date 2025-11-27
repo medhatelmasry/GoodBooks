122ad9218e205c547d53b6f699f462a370ffe5da
jefferymj <jefferymjoseph@gmail.com>
2025-11-27 14:03:47 -0800

    Bug fix. Updated Aspire sdk to v13.0.1.

--- src/src.AppHost/Program.cs
+++ src/src.AppHost/Program.cs
@@ -4,7 +4,8 @@ using Google.Protobuf.WellKnownTypes;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var sqlServer = builder.AddSqlServer("gdb-sql-server")
-    .WithImage("mcr.microsoft.com/mssql/server", "2022-latest")
+    .WithImageRegistry("mcr.microsoft.com")
+    .WithImage("mssql/server", "2022-latest")
     .WithEnvironment("ACCEPT_EULA", "Y")
     .WithEnvironment("MSSQL_SA_PASSWORD", "YourStrong!Passw0rd")
     .AddDatabase("gdb-db");
@@ -12,11 +13,11 @@ var sqlServer = builder.AddSqlServer("gdb-sql-server")
 // read environment variable for connection string
 var apiService = builder.AddProject<Projects.Api>("api")
         .WithReference(sqlServer)
-        .WithHttpEndpoint(port: 8001)
+        .WithHttpEndpoint(port: 8001, name: "api-http")
         .WaitFor(sqlServer);
 
 builder.AddProject<Projects.AccountGoWeb>("mvc")
-        .WithHttpEndpoint(port: 8000)
+        .WithHttpEndpoint(port: 8000, name: "mvc-http")
         .WithReference(apiService)
         .WaitFor(apiService);
 
