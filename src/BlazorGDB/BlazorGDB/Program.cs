29c84c7c50c2b197c855e0edda416ab35dcae6e9
jefferymj <jefferymjoseph@gmail.com>
2025-12-01 18:41:28 -0800

    Bug fix: Update BlazorGDB to use MapStaticAssets() for wasm support

--- src/BlazorGDB/BlazorGDB/Program.cs
+++ src/BlazorGDB/BlazorGDB/Program.cs
@@ -7,6 +7,8 @@ using BlazorGDB.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
@@ -54,11 +56,10 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
+app.MapStaticAssets();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
