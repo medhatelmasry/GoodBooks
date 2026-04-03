7caeb9b89ad1bdcb0f7d9ca610d9b74273e39c45
EJaySant <ervinjs06@gmail.com>
2026-04-03 15:32:49 -0700

    feat: added loadNavbarState & saveNavbarState in navbar.js to save the expanded state of the MVC navigation menu

--- src/AccountGoWeb/wwwroot/js/navbar.js
+++ src/AccountGoWeb/wwwroot/js/navbar.js
@@ -1,5 +1,30 @@
-$('.sub-menu ul').hide();
+function loadNavbarState() {
+    const navbarState = localStorage.getItem("navbarState");
+    if (navbarState) {
+        const expandedNavbar = JSON.parse(navbarState);
+        expandedNavbar.forEach(index => {
+            $(".sub-menu").eq(index).children("ul").show();
+            $(".sub-menu").eq(index).find(".ml-auto").toggleClass("fa-caret-up fa-caret-down");
+        })
+    }
+}
+
+function saveNavbarState() {
+    const expandedNavbar = [];
+    $(".sub-menu").each(function (index) {
+        if ($(this).children("ul").is(":visible")) {
+            expandedNavbar.push(index);
+        }
+    });
+    localStorage.setItem("navbarState", JSON.stringify(expandedNavbar));
+}
+
+$(".sub-menu ul").hide();
 $(".sub-menu a").click(function () {
-    $(this).parent(".sub-menu").children("ul").slideToggle("100");
-    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+    $(this).parent(".sub-menu").children("ul").slideToggle("100", function () {
+        saveNavbarState();
+    });
+    $(this).find(".ml-auto").toggleClass("fa-caret-up fa-caret-down");
 });
+
+loadNavbarState();
